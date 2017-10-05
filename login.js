91e4246fb6c974b4d57188edecf5637872d690b0
Sarthak Sarangi <sarthaks793@gmail.com>
2017-10-05 14:53:30 +0530

    Update login.js

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ function validate()
 	if ( username == "Sarthak" && password == "Sarthak")
 	{
 	alert ("Login successful");
-	window.location.href = "name.html"; 
+	window.location.href = "https://sweezy793.github.io/name"; 
 	return false;
 	}
 	else if(username.value.length==0 && password.value.length==0)
